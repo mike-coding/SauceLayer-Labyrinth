d759a152c2c823414750531c4a279dc859535f4b
mike-wurth <mike-wurth@uiowa.edu>
2023-04-01 16:09:51 -0500

    updated directory n allat

---
+++ Assets/playerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    float Horizontal;
+    float Vertical;
+    Rigidbody2D body;
+    public float MoveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        Vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        MoveDirection();
+    }
+
+    void MoveDirection()
+    {
+
+        float moveLimiter = 0.75f;
+        if (Horizontal != 0 && Vertical != 0)
+        {
+            Horizontal *= moveLimiter;
+            Vertical *= moveLimiter;
+        }
+
+        //set body velocity
+        body.velocity = new Vector2(Horizontal * MoveSpeed, Vertical * MoveSpeed);
+
+
+    }
+}
